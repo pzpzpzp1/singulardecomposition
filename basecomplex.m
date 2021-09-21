7c844c75d5b334d225d69df4894b74aff5d0ae1b
hsinhui2 <55320139+hsinhui2@users.noreply.github.com>
2021-09-20 22:42:10 -0500

    ismember

--- basecomplex.m
+++ basecomplex.m
@@ -14,16 +14,21 @@ nF = size(F,1);
 %if they are adjacent to 2 hexes, interior. 
 Fb =[];  %boundary
 Fi =[];  %interior
-for i=1:nH
-    for j=1:nH
-        [a,(i,j)] = ismember([F(i,:),F(i,:)], [sort(H(i,:),8),'rows',sort(H(j,:),8),'rows']); 
-        if a == 1
+for j=1:nH
+    for i=1:nF
+        [a,~] = ismember(F(i,:), sort(H(j,:),8),'rows'); 
+        [b,~] = ismember(F(i,:), sort(H(j,:),8),'rows'); %want to change this
+        if a == 1 && b == 1
             Fb = [Fb; F(i,:)];
         else
             Fi = [Fi; F(i,:)];
+            F = [F((1,:):(i-1,:)),F((i,:):(i-1,:))
         end
     end
 end
+
+%extract the vertices from Fi
+
 
         
 end
