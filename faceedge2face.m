2a5f2444a930d9e22f3df38fca7b9ecf47df7705
hsinhui2 <55320139+hsinhui2@users.noreply.github.com>
2021-11-02 17:02:04 -0500

    discussion

--- faceedge2face.m
+++ faceedge2face.m
@@ -1,1 +1,18 @@
-[F2] = faceedge2face(F1, E12)
+function [Fdata] = hexface2hex(F1, E12, F)
+    Fdata(1)=F1;
+    nH = size(H,1);
+    
+    for j =1:nH %need to know how to stop/identify boundary
+    [~,Hi] = ismember(sort(H1,2), sort(H,2), 'rows');
+    H(Hi,:) = [];
+    [~,H2] = ismember(sort(F12(:),2), sort(H,2), 'rows');
+    F = hex2face(H2);
+    for i=1:6
+    [Y,F2] = ismember(sort(F(i,:),2),sort(F12,2), 'rows');
+    if Y==zeros(4)
+        F12=F2;
+    end
+    end
+    Hdata(j+1) = Hdata;
+    end
+end
--- hexface2hex.m
+++ hexface2hex.m
@@ -6,12 +6,7 @@ function [Hdata] = hexface2hex(H1, F12, H)
     [~,Hi] = ismember(sort(H1,2), sort(H,2), 'rows');
     H(Hi,:) = [];
     [~,H2] = ismember(sort(F12(:),2), sort(H,2), 'rows');
-    F = hex2face(H2);
-    for i=1:6
-    [Y,F2] = ismember(sort(F(i,:),2),sort(F12,2), 'rows');
-    if Y==zeros(4)
-        F12=F2;
-    end
+   
     end
     Hdata(j+1) = Hdata;
     end
