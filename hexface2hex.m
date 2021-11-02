e2ac1c05bd06da17f64f8969756064753f64f712
hsinhui2 <55320139+hsinhui2@users.noreply.github.com>
2021-11-02 07:46:28 -0500

    hex data structure

--- hexface2hex.m
+++ hexface2hex.m
@@ -1,15 +1,16 @@
-function [H2] = hexface2hex(H1, F12, H)
+function [Hdata] = hexface2hex(H1, F12, H)
+    Hdata=[H1];
+    for j =1:nH
     [~,Hi] = ismember(sort(H1,2), sort(H,2), 'rows');
     H(Hi,:) = [];
     [~,H2] = ismember(sort(F12(:),2), sort(H,2), 'rows');
-end
-    
-function [F23] = facehex2face(H2,F12)
     F = hex2face(H2);
     for i=1:6
     [Y,F2] = ismember(sort(F(i,:),2),sort(F12,2), 'rows');
-    if Y=0
-        F23=F2;
+    if Y==zeros(4)
+        F12=F2;
     end
+    end
+    Hdata = [Hdata, H2];
     end
 end
