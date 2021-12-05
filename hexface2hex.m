070f39c6e502dc92082de9977c7fdde65e694819
hsinhui2 <55320139+hsinhui2@users.noreply.github.com>
2021-12-05 12:15:52 -0600

    add hexface2hex graph

--- hexface2hex.m
+++ hexface2hex.m
@@ -8,6 +8,7 @@ function [H2] = hexface2hex(H1, F12, H,V, visualize)
     H1 = H(2,:); %setup
     [~,~,F,H,~,~,nF,nH] = datastructure(V,H);
     F12 = F(2,:); %setup
+    visualize =1;
     end
     [V,E,F,H] = datastructure(V,H);
     [~,Hi] = ismember(sort(H1,2), sort(H,2), 'rows'); %to find the index of the one to exclude
@@ -22,8 +23,21 @@ function [H2] = hexface2hex(H1, F12, H,V, visualize)
     Fi = Fi';
     Hj = rem(Fi,nH); 
     H2 = setdiff(Hj,Hi); %output is 0 needs to be 4 (add nH?)
+    %if 0 and turn to 4
     if H2 == 0
         H2 = nH;
     end
-    %if 0 and turn to 4
+    Vh1 = V(H1,:);
+    Vh2 = V(H(H2,:),:);
+    Fh = hex2face(H(H2,:));
+
+    %graph
+    if visualize == 1
+        figure; title('hexface2hex'); axis equal; hold all; rotate3d on;
+        patch('Faces', F, 'Vertices', V, 'facecolor', 'blue', 'facealpha', 0.1);
+        scatter3(Vh1(:,1), Vh1(:,2), Vh1(:,3), 'b', 'filled');
+        patch('Faces', F12, 'Vertices', V, 'facecolor', 'green', 'facealpha', 0.1);
+        patch('Faces', Fh, 'Vertices', V, 'facecolor', 'red', 'facealpha', 0.1);
+        scatter3(Vh2(:,1), Vh2(:,2), Vh2(:,3), 'k', 'filled');
+    end
 end
