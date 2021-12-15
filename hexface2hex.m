e459867a46f98b6c02e5d12ee98706c21bc9fdf0
hsinhui2 <55320139+hsinhui2@users.noreply.github.com>
2021-12-15 15:00:49 -0600

    updated version with data structure

--- hexface2hex.m
+++ hexface2hex.m
@@ -1,29 +1,25 @@
-function [H2] = hexface2hex(H1, F12, H,V, visualize)
+function [H2] = hexface2hex(H1, F12, data, visualize)
 %do visualization work
     if nargin==0
     file_name = 'sing1.vtk';
     mesh = load_vtk(file_name);
     V = mesh.points;
     H = mesh.cells;
-    H1 = H(2,:); %setup
-    [~,~,F,H,~,~,nF,nH] = datastructure(V,H);
-    F12 = F(2,:); %setup
+    data = builddatastruct(H,V); F = data.F;
+    %setup
+    H1 = H(2,:); 
+    F12 = F(2,:);
     visualize =1;
     end
-    [V,E,F,H] = datastructure(V,H);
+    data = builddatastruct(H,V);
+    E = data.Eh; F = data.Fh; H = data.H; nH = data.nH;
     [~,Hi] = ismember(sort(H1,2), sort(H,2), 'rows'); %to find the index of the one to exclude
     %ismember stops after finding one valid choice
     Fi = find(all((sort(F12, 2)- sort(F, 2))==0, 2));
-%     Fi = [];
-%     for i = 1:nF
-%        if sort(F12, 2)-sort(F(i,:), 2)==0
-%            Fi = [Fi; i];
-%        end
-%     end
     Fi = Fi';
     Hj = rem(Fi,nH); 
-    H2 = setdiff(Hj,Hi); %output is 0 needs to be 4 (add nH?)
-    %if 0 and turn to 4
+    H2 = setdiff(Hj,Hi); 
+    %output is 0 needs to be 4 - if 0 and turn to 4
     if H2 == 0
         H2 = nH;
     end
